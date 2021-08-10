429f0ef0c30615c1f4246325c4157b6e3115b57f
Mike Dehn <mwdehn@gmail.com>
2021-08-10 16:43:50 -0400

    adding evaluation of model as part of training process

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import argparse
 import datetime
-from gan import discriminator, generator, composite, cyclegan
+from gan import discriminator, generator, cyclegan
 import numpy as np
 from matplotlib import pyplot as plt
 import tensorflow as tf
@@ -8,7 +8,6 @@ from tensorflow import keras
 from tensorflow.keras.utils import plot_model
 from tensorflow_addons.layers import InstanceNormalization
 
-tf.compat.v1.enable_eager_execution()
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 
 def generate_real_images(dataset, n_samples, patch_shape):
@@ -37,34 +36,6 @@ def update_image_pool(pool, images, max_size=50):
     
     return np.asarray(selected)
 
-def train(d_model_A, d_model_B, g_model_AtoB, g_model_BtoA, c_model_AtoB, c_model_BtoA, trainA, trainB, n_steps, n_batch):
-    n_patch = d_model_A.output_shape[1]
-
-    poolA = list()
-    poolB = list()
-
-    for i in range(n_steps):
-        X_realA, y_realA = generate_real_images(trainA, n_batch, n_patch)
-        X_realB, y_realB = generate_real_images(trainB, n_batch, n_patch)
-
-        X_fakeA, y_fakeA = generate_fake_images(g_model_BtoA, X_realB, n_patch)
-        X_fakeB, y_fakeB = generate_fake_images(g_model_AtoB, X_realA, n_patch)
-
-        X_fakeA = update_image_pool(poolA, X_fakeA)
-        X_fakeB = update_image_pool(poolB, X_fakeB)
-
-        g_loss2, _, _, _, _ = c_model_BtoA.train_on_batch([X_realB, X_realA], [y_realA, X_realA, X_realB, X_realA])
-
-        dA_loss1 = d_model_A.train_on_batch(X_realA, y_realA)
-        dA_loss2 = d_model_A.train_on_batch(X_fakeA, y_fakeA)
-
-        g_loss1, _, _, _, _ = c_model_AtoB.train_on_batch([X_realA, X_realB], [y_realB, X_realB, X_realA, X_realB])
-
-        dB_loss1 = d_model_B.train_on_batch(X_realB, y_realB)
-        dB_loss2 = d_model_B.train_on_batch(X_fakeB, y_fakeB)
-
-        print('>%d, dA[%.3f,%.3f] dB[%.3f,%.3f] g[%.3f,%.3f]' % (i+1, dA_loss1,dA_loss2, dB_loss1,dB_loss2, g_loss1,g_loss2))
-
 IMAGE_SIZE = [256, 256]
 
 def decode_image(image):
@@ -89,40 +60,6 @@ def load_data(filenames, labeled=True, ordered=False):
     return dataset
 
 def train_main(n_epochs, n_batch, dropout):
-    dimensions = (256,256,3)
-
-    MONET_FILENAMES = tf.io.gfile.glob('./monet_tfrec/*.tfrec')
-    PHOTO_FILENAMES = tf.io.gfile.glob('./photo_tfrec/*.tfrec')
-    monet_data = load_data(MONET_FILENAMES, labeled=True)
-    photo_data = load_data(PHOTO_FILENAMES, labeled=True)
-    dataset = (monet_data, photo_data)
-
-    trainA, trainB = dataset
-    trainA = np.asarray(list(trainA.as_numpy_iterator()))
-    trainB = np.asarray(list(trainB.as_numpy_iterator()))
-
-    bat_per_epoch = int(len(list(trainA)) / n_batch)
-
-    n_steps = bat_per_epoch * n_epochs
-    print(f"n_steps = {n_steps}")
-
-    #lr = learning_rate.GanLearningRateSchedule(0.0002, n_steps)
-    #lr = 0.0002
-
-    g_model_AtoB = generator.build(dimensions, 9, dropout)
-    g_model_BtoA = generator.build(dimensions, 9, dropout)
-    d_model_A = discriminator.build(dimensions, tf.keras.optimizers.schedules.PolynomialDecay(0.0002, n_steps, 0))
-    d_model_B = discriminator.build(dimensions, tf.keras.optimizers.schedules.PolynomialDecay(0.0002, n_steps, 0))
-
-    c_model_AtoBtoA = composite.build(g_model_AtoB, d_model_B, g_model_BtoA, dimensions, tf.keras.optimizers.schedules.PolynomialDecay(0.0002, n_steps, 0))
-    c_model_BtoAtoB = composite.build(g_model_BtoA, d_model_A, g_model_AtoB, dimensions, tf.keras.optimizers.schedules.PolynomialDecay(0.0002, n_steps, 0))
-
-    train(d_model_A, d_model_B, g_model_AtoB, g_model_BtoA, c_model_AtoBtoA, c_model_BtoAtoB, trainA, trainB, n_steps, n_batch)
-
-    g_model_BtoA.save(f"photo_to_monet_model_e{n_epochs}_b{n_batch}_d{int(dropout * 100)}")
-    g_model_AtoB.save(f"monet_to_photo_model_e{n_epochs}_b{n_batch}_d{int(dropout * 100)}")
-
-def new_train(n_epochs, n_batch, dropout):
     dimensions = [256, 256, 3]
 
     MONET_FILENAMES = tf.io.gfile.glob('./monet_tfrec/*.tfrec')
@@ -142,31 +79,42 @@ def new_train(n_epochs, n_batch, dropout):
 
     cycle_model.fit(tf.data.Dataset.zip((monet_data, photo_data)), epochs=n_epochs, callbacks=[tensorboard_callback])
     
-    g_model_AtoB.save(f"new_photo_to_monet_model_e{n_epochs}_b{n_batch}_d{int(dropout * 100)}")
-    g_model_BtoA.save(f"new_monet_to_photo_model_e{n_epochs}_b{n_batch}_d{int(dropout * 100)}")
+    g_model_AtoB.save(f"p2m_model_e{n_epochs}_b{n_batch}_d{int(dropout * 100)}")
+    g_model_BtoA.save(f"m2p_model_e{n_epochs}_b{n_batch}_d{int(dropout * 100)}")
 
-def eval_main(model_name, out_path, n=20):
-    model = keras.models.load_model(model_name, compile=False)
+    evaluate_p2m(g_model_AtoB, f"out_p2m_e{n_epochs}_b{n_batch}_d{int(dropout * 100)}.png", 100)
+    evaluate_m2p(g_model_BtoA, f"out_m2p_e{n_epochs}_b{n_batch}_d{int(dropout * 100)}.png", 100)
 
-    model.compile()
-
-    PHOTO_FILENAMES = tf.io.gfile.glob('./photo_tfrec/*.tfrec')
-    photo_data = load_data(PHOTO_FILENAMES, labeled=True).batch(1)
-
+def evaluate_results(model, out_path, n, data):
     _, ax = plt.subplots(n, 2, figsize=(12, 5 * n))
-    for i, img in enumerate(photo_data.take(n)):
+    for i, img in enumerate(data.take(n)):
         prediction = model(img, training=False)[0].numpy()
         prediction = (np.asarray(prediction) * 127.5 + 127.5).astype(np.uint8)
         img = (img[0] * 127.5 + 127.5).numpy().astype(np.uint8)
 
         ax[i, 0].imshow(img)
         ax[i, 1].imshow(prediction)
-        ax[i, 0].set_title("Photo")
-        ax[i, 1].set_title("Fake Monet")
+        ax[i, 0].set_title("Original")
+        ax[i, 1].set_title("Fake")
         ax[i, 0].axis("off")
         ax[i, 1].axis("off")
     
     plt.savefig(out_path)
+
+def evaluate_p2m(model, out_path, n):
+    photo_files = tf.io.gfile.glob('./photo_tfrec/*.tfrec')
+    photo_data = load_data(photo_files, labeled=True).batch(1)
+    evaluate_results(model, out_path, n, photo_data)
+
+def evaluate_m2p(model, out_path, n):
+    monet_files = tf.io.gfile.glob('./monet_tfrec/*.tfrec')
+    monet_data = load_data(monet_files, labeled=True).batch(1)
+    evaluate_results(model, out_path, n, monet_data)
+
+def eval_main(model_name, out_path, n=20):
+    model = keras.models.load_model(model_name, compile=False)
+    model.compile()
+    evaluate_p2m(model, out_path, n)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Driver for training Monet art generator")
@@ -185,6 +133,6 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.command == "train":
-        new_train(args.epoch, args.batch, args.dropout)
+        train_main(args.epoch, args.batch, args.dropout)
     elif args.command == "eval":
         eval_main(args.model_name, args.output, args.n_samples)
